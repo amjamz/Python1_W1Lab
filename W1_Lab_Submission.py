0cceec528188dc1600a032ab270765b7a347ecd0
amjamz <164314207+amjamz@users.noreply.github.com>
2024-03-22 16:58:58 -0400

    Create W1_Lab_Submission.py

---
+++ W1_Lab_Submission.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Python I Week 1 Lab
+"""
+
+"Instructions:"
+" Your lab assignment for all of week 1:"
+"Install and test Python"
+"Install and test GitHub Desktop"
+"Create your own GitHub repo and add a file to it (a code file, a text file," 
+"n image, a csv doc, etc).  If you work in a group, one person should create"
+"it and then add access for the others (under settings), and everyone should"
+"make at least one commit.  Don't forget to write clear commit messages and"
+"then push the commits!"
+"You will turn in the URL to your GitHub repo here on Canvas."
+
+print("Hello, world!")
+print("Hello, grader!")
